5750c646fc4d67799726d1b01646630677d12a56
Nick Ellis <nicholas.w.ellis@gmail.com>
2019-04-29 23:04:11 -0500

    add gilde module for app

---
+++ app/src/main/java/me/nickellis/caturday/CaturdayGlideModule.kt
@@ -0,0 +1,10 @@
+package me.nickellis.caturday
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+
+@GlideModule
+class CaturdayGlideModule : AppGlideModule() {
+
+}
