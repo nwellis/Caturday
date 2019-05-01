8c1a4bae7fd151b03dd9ccd65870f35b72b27cfc
Nick Ellis <nicholas.w.ellis@gmail.com>
2019-04-30 21:46:28 -0500

    add a children ktx function for view group

--- app/src/main/java/me/nickellis/caturday/ktx/View.kt
+++ app/src/main/java/me/nickellis/caturday/ktx/View.kt
@@ -1,9 +1,13 @@
 package me.nickellis.caturday.ktx
 
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.core.view.size
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 
+fun <T : ViewGroup> T.children(): List<View> = (0 until this.childCount).map { index -> getChildAt(index) }
 
 fun <T : ImageView> T.loadImageFromWeb(url: String?): T {
   Glide.with(this)
