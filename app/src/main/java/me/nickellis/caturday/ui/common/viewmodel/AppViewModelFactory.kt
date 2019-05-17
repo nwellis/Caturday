07b4971ed19bd97595e853cbbedbad2ae6ed9579
Nick Ellis <nicholas.w.ellis@gmail.com>
2019-05-16 19:44:00 -0500

    add license to view model factory as well

--- app/src/main/java/me/nickellis/caturday/ui/common/viewmodel/AppViewModelFactory.kt
+++ app/src/main/java/me/nickellis/caturday/ui/common/viewmodel/AppViewModelFactory.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package me.nickellis.caturday.ui.common.viewmodel
 
 import androidx.lifecycle.ViewModel
@@ -14,7 +30,8 @@ import javax.inject.Singleton
 @Singleton
 class AppViewModelFactory  @Inject constructor(
   private val creators: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
-) : ViewModelProvider.Factory {
+): ViewModelProvider.Factory {
+
   override fun <T : ViewModel> create(modelClass: Class<T>): T {
     val creator = creators[modelClass] ?: creators.entries.firstOrNull {
       modelClass.isAssignableFrom(it.key)
